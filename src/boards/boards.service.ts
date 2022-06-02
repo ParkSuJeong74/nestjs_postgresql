b0258839bbf5dd54d2d69b10a4db28a9ae50c740
ParkSuJeong <sujeong9158@gmail.com>
2022-06-02 13:34:48 +0900

    [feat] pipe setting

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +12,8 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
@@ -19,6 +22,27 @@ export class BoardsService {
     };
 
     this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`); // 유효성 체크
+    }
+    return found;
+  }
+
+  deleteBoard(id: string): void {
+    // return 값 없음
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
     return board;
   }
 }
