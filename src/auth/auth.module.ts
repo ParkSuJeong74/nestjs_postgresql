a020bf25f8adbc5a4da980cd717b40422495f704
ParkSuJeong <sujeong9158@gmail.com>
2022-06-04 19:49:51 +0900

    [feat] jwt token 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from '../repository/user.repository';
+import { DatabaseModule } from 'src/database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config';
+import { PassportModule } from '@nestjs/passport';
 
+const jwtConfig = config.get('jwt');
 @Module({
+  imports: [
+    DatabaseModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConfig.secret,
+      signOptions: {
+        expiresIn: 60 * 60, // 한시간
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [...UserRepository, AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSaltSync(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      username,
+      password: hashPassword,
+    });
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+    return user;
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({
+      where: {
+        username,
+      },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
+  }
+}
